7e8356aafeec571eb26e50db4a553b00cb4dd678
Joshua Rizek <joshuarizek@gmail.com>
2022-09-15 20:02:24 -0400

    removing unnecessary folders and adding font awesome package

--- counter-app2/src/index.js
+++ counter-app2/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css' 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Counter from './components/counter';
+import 'font-awesome/css/font-awesome.css'
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
