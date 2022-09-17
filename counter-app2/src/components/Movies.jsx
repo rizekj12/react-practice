478e42f5d489031c61fcade25d40105a63ccc163
Joshua Rizek <joshuarizek@gmail.com>
2022-09-17 17:04:34 -0400

    started delete function

--- counter-app2/src/components/Movies.jsx
+++ counter-app2/src/components/Movies.jsx
@@ -5,31 +5,38 @@ class Movies extends Component {
     state = {
         movies: getMovies()
     }
+
+    deleteMovie = movie => {
+        this.setState(this.state.movies)
+        console.log(movie)
+    }
+
     render() {
-        console.log(this.state.movies)
         return (
             <>
                 <h1>Movies component</h1>
                 <table className="table">
-  <thead>
-    <tr>
-      <th scope="col">Title</th>
-      <th scope="col">Genere</th>
-      <th scope="col">Stock</th>
-      <th scope="col">Rate</th>
-    </tr>
-  </thead>
-  <tbody>
-    {this.state.movies.map(movie => (
-            <tr>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-            </tr>
-        ))}
-  </tbody>
-</table>
+                    <thead>
+                        <tr>
+                            <th scope="col">Title</th>
+                            <th scope="col">Genere</th>
+                            <th scope="col">Stock</th>
+                            <th scope="col">Rate</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.movies.map(movie => (
+                            <tr key={movie._id}>
+                                <td>{movie.title}</td>
+                                <td>{movie.genre.name}</td>
+                                <td>{movie.numberInStock}</td>
+                                <td>{movie.dailyRentalRate}</td>
+                                <td><button onClick={() => this.deleteMovie(movie)} className="btn btn-danger">Delete</button></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </>
         );
     }
